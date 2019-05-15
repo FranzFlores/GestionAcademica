13e6ddecf1d63719f764ebedeb7a93d9d661923b
Franz Flores <andresfloresgallardo@gmail.com>
2019-05-14 20:55:53 -0500

    Creacion de Componentes para el login

--- frontend/src/app/models/teacher.ts
+++ frontend/src/app/models/teacher.ts
@@ -1,8 +1,16 @@
 import { Person } from './person';
 
-export class Account {
-    constructor(_id = '', university_degree = '', fourth_level_degree= '',timetable='', person = new Person()) {
+export class Teacher {
+    constructor(dni_person = '', name = '',gender = '',birthday = '', institutional_mail = '',personal_mail = '',address='',phone='',_id = '', university_degree = '', fourth_level_degree= '',timetable='', person = new Person()) {
         this._id = _id;
+        this.dni_person = dni_person;
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.institutional_mail = institutional_mail;
+        this.personal_mail = personal_mail;
+        this.address = address;
+        this.phone = phone;
         this.university_degree = university_degree;
         this.fourth_level_degree = fourth_level_degree;
         this.timetable = timetable;
@@ -10,6 +18,14 @@ export class Account {
     }
     
     _id: string;
+    dni_person: String;
+    name: String;
+    gender: String;
+    birthday: String;
+    institutional_mail:String;
+    personal_mail: String;
+    address:String;
+    phone: String;
     university_degree: string;
     fourth_level_degree: string;
     timetable:String;
---
+++ frontend/src/app/services/teacher.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+import {Teacher} from "../models/teacher";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherService {
+
+  //person : Person;
+  teacher : Teacher;
+
+  readonly URL_API = 'http://localhost:3000/api/person';
+
+  constructor(private http: HttpClient) {
+    this.teacher = new Teacher();
+  }
+
+  postTeacher(teacher:Teacher){
+    return this.http.post(this.URL_API + "/create",teacher);
+  }
+
+  
+
+
+  
+
+}
